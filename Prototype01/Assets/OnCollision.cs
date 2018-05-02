8f5839aa350dede9fc1e446cc3667eb674ecdb4b
LaDwan <ladwanhester@gmail.com>
2018-05-01 22:37:14 -0400

    first commit

---
+++ Prototype01/Assets/OnCollision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollision : MonoBehaviour {
+    public GameObject CanvasText;
+	// Use this for initialization
+	void Start () {
+        CanvasText.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Dud")
+        {
+            CanvasText.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("Tag has failed");
+        }
+    }
+}
---
+++ Prototype01/Assets/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject Player;
+    public int movespeed;
+    public Vector3 userDirection = Vector3.right;
+    public void Start()
+    {
+
+    }
+
+
+    public void Update()
+    {
+        Debug.Log(Input.acceleration.x);
+        transform.Translate(userDirection * movespeed * Time.deltaTime);
+
+        if (Input.acceleration.x > 0.02)
+        {
+            Player.GetComponent<Rigidbody2D>().gravityScale = -1;
+        }
+
+        else
+        {
+            Player.GetComponent<Rigidbody2D>().gravityScale = 1;
+        }
+
+    }
+}
